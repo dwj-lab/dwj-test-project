c110f8f97cee1a88f5a17e34c06e7fa288803589
Jonathan Gilligan <jonathan.gilligan@vanderbilt.edu>
2021-07-08 16:26:08 -0500

    Include eye color in starwars plot.

--- starwars.R
+++ starwars.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 data(starwars)
 
-ggplot(starwars, aes(x = height, y = mass, color = species, shape = gender)) +
+ggplot(starwars, aes(x = height, y = mass, color = eye_color, shape = gender)) +
   geom_point(size = 2) +
   scale_y_log10() +
   labs(x = "Height (cm)", y = "Mass (kg)", title = "Starwars Characters") +
