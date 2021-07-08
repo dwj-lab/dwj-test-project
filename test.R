465f982fce6a8e4cb9299c550170d10ca64ae77a
Jonathan Gilligan <jonathan.gilligan@vanderbilt.edu>
2021-07-08 16:07:14 -0500

    Updated iris plot and added a new starwars plot.

--- test.R
+++ test.R
@@ -1,11 +1,13 @@
  library(tidyverse)
- 
- ggplot(mtcars, aes(x = disp, y = mpg, color = ordered(gear), 
+
+ ggplot(mtcars, aes(x = disp, y = mpg, color = ordered(gear),
                     shape = ordered(cyl))) +
    geom_point(size = 2) +
    scale_color_brewer(palette = "Dark2") +
    theme_bw()
 
- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) + 
-    geom_point(size = 2) + 
+ ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+    geom_point(size = 3) +
+    scale_color_brewer(palette = "Dark2") +
+    labs(x = "Length", y = "Width", title = "Sepal characteristics of irises") +
     theme_bw()
